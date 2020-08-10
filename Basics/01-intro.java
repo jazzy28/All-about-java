86c7f5acea6fe39fbf705b10a84d7223fd7cc356
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-08-11 03:11:44 +0530

    Create 01-intro.java

---
+++ Basics/01-intro.java
@@ -0,0 +1,9 @@
+import java.util.*;
+//name of class and name of program is same
+public class MyFirstProgram{
+
+public static void main(String[] args) {
+		System.out.print("Hello World\n"); //prints in next line
+		System.out.println("Hello World"); // this too prints in next line
+	}
+}
