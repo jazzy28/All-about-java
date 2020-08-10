9e97b24091352477ba5b0adb38deccd6a3fb07bd
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-08-11 03:12:24 +0530

    Add files via upload

---
+++ Basics/03-Trianglebetter.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Trianglebetter{
+public static void main(String[] args) {
+		System.out.println("*\n**\n***\n****\n*****\n");
+		System.out.println(10); // printing numbers does not require inverted commas
+		//System.out.print("10") and System.out.print(10) will have the same output.
+	}
+}
+//prints triangle in 1 line
+
+//Question: do for equilateral traingle and Z
