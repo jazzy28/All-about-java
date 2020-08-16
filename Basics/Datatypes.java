7e18624b1d949f847ad0c425d68e253a6a0395bb
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-08-16 16:54:31 +0530

    Create Datatypes.java

---
+++ Basics/Datatypes.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main{
+public static void main(String[] args){
+int x = 5;
+
+float y = 7.9f; //f is necessary
+double z = 7.9835;
+
+boolean iAmCool = true;
+
+System.out.println(x); //5
+System.out.println(y); //7.9
+System.out.println(z); //7.9835
+System.out.println(iAmCool); //true
+
+  }
+}
