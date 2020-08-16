04ebe734ddc9c28633cdb698b828921b21061e2f
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-08-16 14:55:47 +0530

    Create 00-HelloWorld.java

---
+++ Basics/00-HelloWorld.java
@@ -0,0 +1,8 @@
+//name of the class and filename should be same. Otherwise, it wil not prints the output
+
+public class HelloWorld{
+
+     public static void main(String []args){
+        System.out.println("Hello World");
+     }
+}
