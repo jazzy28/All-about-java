145704c6efeb7ebd43b1cf81843a5630899288a2
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-08-17 10:56:47 +0530

    Create 23-TypeCasting.java

---
+++ Basics/23-TypeCasting.java
@@ -0,0 +1,12 @@
+//Automatic Typecasting: Widening
+import java.util.*;
+
+public class TypeCasting{
+
+     public static void main(String []args){
+        int x = 4;
+        double y = x;
+        System.out.println(y);
+        
+     }
+}
