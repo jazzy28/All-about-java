13efde9f66259ff4b8b1ff38a10ca98e9c9c700b
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-08-23 22:48:06 +0530

    Create 24-manualTypeCasting.java

---
+++ Basics/24-manualTypeCasting.java
@@ -0,0 +1,16 @@
+//Manual Typecasting: Narrowing
+import java.util.*;
+
+public class manualTypeCasting{
+
+     public static void main(String []args){
+        // int x = 4;
+        // double y = x;
+        // System.out.println(y);
+        double myDouble = 2.82;
+        int myInt = (int)myDouble; //manual
+        System.out.println(myDouble);
+         System.out.println(myInt);
+        
+     }
+}
