25566a486ce876413e175ec6561a1af1f63456c2
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-09-06 02:41:37 +0530

    Create 19-printArrayEle.java

---
+++ Functions-and-Arrays/19-printArrayEle.java
@@ -0,0 +1,11 @@
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+       //printing element instead of index
+       int[] nums = {3,4,56,78,90,2};
+       for(int num: nums){
+           System.out.println(num);
+       }
+    }
+}
