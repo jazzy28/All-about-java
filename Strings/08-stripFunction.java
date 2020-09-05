58e6a03c8afade9af54effbc4e11ede7aed36ae8
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-09-06 02:35:10 +0530

    Update 08-stripFunction.java

--- Strings/08-stripFunction.java
+++ Strings/08-stripFunction.java
@@ -9,5 +9,13 @@ public class Main
         System.out.println( str.stripLeading() );   //"Hello World !!   "
  
         System.out.println( str.stripTrailing() );  //"  Hello World !!"
+        
+ 
+        System.out.println(str.toUpperCase()); 
+        System.out.println(str.toLowerCase()); 
+        System.out.println(str.toLowerCase());
+        System.out.println(str.replace('a', 'e')); //as strings are immutable it cannot be changed
+       str = str.replace('e', 'a');
+       System.out.println(str);
     }
 }
