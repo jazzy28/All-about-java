3576306a34d0008e185e39e57ae01cb5d2a68cf2
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-09-05 19:14:13 +0530

    Create 01-Intro.java

---
+++ Strings/01-Intro.java
@@ -0,0 +1,11 @@
+//String: class, collection of characters
+import java.util.*;
+public class Main{
+
+     public static void main(String []args){
+        String person = "Jasmine";//reference: person, object: Jasmine
+        System.out.println(person);
+        System.out.println(person.charAt(2)); //string: character array, way of retrieval
+
+     }
+}
