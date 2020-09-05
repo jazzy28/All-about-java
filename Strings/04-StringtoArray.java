64790d7a3efda534d1db29a64991ff79ae162cb3
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-09-06 01:28:50 +0530

    Create 04-StringtoArray.java

---
+++ Strings/04-StringtoArray.java
@@ -0,0 +1,10 @@
+//string to array
+import java.util.*;
+public class Main{
+
+     public static void main(String []args){
+        String person = "Jasmine";
+        char[] arr = person.toCharArray();//returns character array
+        System.out.println(Arrays.toString(arr)); 
+     }
+}
