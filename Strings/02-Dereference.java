ad66cdbd69c3ce0b31698d28f18dc942b5345999
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-09-05 19:18:04 +0530

    Create 02-Dereference.java

---
+++ Strings/02-Dereference.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main{
+
+     public static void main(String []args){
+        String person = "Jasmine";
+        person = "Rose";
+        //person[0] = 'E'; not allowed
+        System.out.println(person);
+        
+        //strings are immutable, therefore, can only be dereferenced but not change
+        
+     }
+}
