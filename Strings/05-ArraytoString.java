e255b5b65833d572bbc74d063123815243fe6079
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-09-06 01:41:56 +0530

    Update 05-ArraytoString.java

--- Strings/05-ArraytoString.java
+++ Strings/05-ArraytoString.java
@@ -5,10 +5,12 @@ public class Main{
         String person = "Jasmine";
         char[] arr = person.toCharArray();//returns character array
         System.out.println(Arrays.toString(arr)); 
-        
-        //character array to string
         String name = String.valueOf(arr);
         System.out.println(name);
         System.out.println(person);
+        
+        //Substring
+        System.out.println(name.substring(1));
+        System.out.println(name.substring(2, 7));//start index, end-1
      }
 }
