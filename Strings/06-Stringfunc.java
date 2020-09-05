a27fa478de04e484e74a9c8404da39f88fc8d134
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-09-06 01:53:09 +0530

    Create 06-Stringfunc.java

---
+++ Strings/06-Stringfunc.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main{
+
+     public static void main(String []args){
+        String person = "Jasmine";
+        char[] arr = person.toCharArray();//returns character array
+        System.out.println(Arrays.toString(arr)); 
+        String name = String.valueOf(arr);
+        System.out.println(name);
+        System.out.println(person);
+        
+        //Substring
+        System.out.println(name.substring(1));
+        System.out.println(name.substring(2, 7));//start index, end-1
+        System.out.println(name.length());//length
+        System.out.println(" ".isEmpty());//empty string or not, if there space between "", it is not empty, considering space character
+        System.out.println("    ".isBlank()); //its blank
+     }
+}
