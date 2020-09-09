13f75b970a0383126ad29648323371a6097196ae
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-09-09 18:23:15 +0530

    Create 10-StringInput.java

---
+++ Strings/10-StringInput.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        String str1 = s.next();//token wise i/p: separated by space or line
+        String str2 = s.nextLine();
+        //System.out.println(str1);
+        System.out.println(str2);
+    }
+}
+//i/p: hello world, o/p: hello for s.next()
+
+//i/p: hello world, o/p: hello world for s.nextLine()
