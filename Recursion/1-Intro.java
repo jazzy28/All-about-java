c6966d593a69418ef093f44969727bcadceb8bf4
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-09-11 02:56:16 +0530

    Update 1-Intro.java

--- Recursion/1-Intro.java
+++ Recursion/1-Intro.java
@@ -1,6 +1,16 @@
+//Recursion: divide the problem into sub problems but where you are able to reach the base case
+
 import java.util.*;
 public class Main{
     public static void main(String[] args){
-        
+        int n = 5;
+        print(5);
+    }
+    public static void print(int n){ //returning nothing, so void
+        if(n == 0){//stops, when reaches 0
+            return;
+        }
+        System.out.println(n);
+        print(n-1);//recursive call
     }
 }
