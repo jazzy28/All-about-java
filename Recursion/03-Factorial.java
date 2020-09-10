144c1cef7e64508b58e13ac9526a043e335dad25
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-09-11 03:18:30 +0530

    Create 03-Factorial.java

---
+++ Recursion/03-Factorial.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        int n = 5;
+        System.out.println(factorial(5));
+    }
+    public static int factorial(int n){ 
+        if(n == 0){
+            return 1;
+        }
+        
+        return n*factorial(n-1);
+        
+    }
+}
