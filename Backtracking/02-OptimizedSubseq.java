f2ffae220da0f9c2b5082b976c9f741ac2139bc3
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-09-12 00:10:35 +0530

    Update 02-OptimizedSubseq.java

--- Backtracking/02-OptimizedSubseq.java
+++ Backtracking/02-OptimizedSubseq.java
@@ -1,29 +1,34 @@
+//StringBuilder : object can be edited, whereas, we cannot edit String
 import java.util.*;
+
 public class Main{
     public static void main(String[] args){
         String word = "abc";
         StringBuilder proc = new StringBuilder("");
-        StringBuilder unproc = new StringBuilder(word); //StringBuilder can be edited, String cannot be edited
+        StringBuilder unproc = new StringBuilder(word);
+        
+        //unproc.insert(0, 'a');//adding character at 0th index
+        //System.out.println(unproc);
         
         subseq(proc, unproc);
+        
     }
-    
     private static void subseq(StringBuilder proc, StringBuilder unproc){//processed unprocessed
-        if(unproc.length()==0){//base case
-        if(proc.length()!=0){
-            System.out.println(proc);
-        }
+        if(unproc.length() == 0){//base case
+            if(proc.length() != 0){ //to skip empty
+                System.out.println(proc); 
+            }
             return;
         }
         char ch = unproc.charAt(0);
         
-        unproc.delete(0, 1); //index start and final
-        //removing item in unprocessed and appending in processed
-        proc.append(ch); 
+        unproc.delete(0, 1);//delete 0th index
+        
+        proc.append(ch); //adding to proc, same object
         subseq(proc, unproc);
-        unproc.delete(proc.length()-1, proc.length());
+        proc.delete(proc.length()-1, proc.length());
         
         subseq(proc, unproc);
-        
-        unproc.insert(offset:0, ch); //offset ->index
+        unproc.insert(0, ch);
     }
+}
