6657b62846e103a4da3a298768293ef7f217e2d9
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-09-11 23:56:23 +0530

    Update 01-Subseq.java

--- Backtracking/01-Subseq.java
+++ Backtracking/01-Subseq.java
@@ -1,19 +1,20 @@
 import java.util.*;
+
 public class Main{
     public static void main(String[] args){
-        subseq(proc:"", unproc:"abc");
+        subseq("", "abc");
     }
     private static void subseq(String proc, String unproc){//processed unprocessed
         if(unproc.isEmpty()){//base case
-        if(proc.isEmpty()){
-            System.out.println(proc);
-        }
+            if(!proc.isEmpty()){
+                System.out.println(proc); //to skip empty
+            }
             return;
         }
         char ch = unproc.charAt(0);
         unproc = unproc.substring(1);
         
-        subseq(proc: proc + ch, unproc);
+        subseq(proc + ch, unproc);
         subseq(proc, unproc);
     }
 }
