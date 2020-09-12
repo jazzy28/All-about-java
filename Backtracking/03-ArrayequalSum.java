ed9626b1a919fecfc55a764e581307c13f3cdd4e
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-09-12 12:47:50 +0530

    Update 03-ArrayequalSum.java

--- Backtracking/03-ArrayequalSum.java
+++ Backtracking/03-ArrayequalSum.java
@@ -1,29 +1,33 @@
+//StringBuilder : object can be edited, whereas, we cannot edit String
 import java.util.*;
+
 public class Main{
     public static void main(String[] args){
-        int[] nums = {1, 2, 3};
+        int[] nums = {1, 2, 3}; //Array
         
         ArrayList<Integer> first = new ArrayList<>();
         ArrayList<Integer> second = new ArrayList<>();
         
-        eqSum(first, second, fsum:0, ssum:0, nums, index:0)
+        eqSum(first, second, 0, 0, nums, 0);
+        
     }
-    
-    private static void eqSum(ArrayList<Integer> first, ArrayList<Integer> second, int fsum, int ssum, int[] nums, int index){
-        if(index == nums.length){//base case
+    public static void eqSum(ArrayList<Integer> first, ArrayList<Integer> second, int fsum, int ssum, int[] nums, int index){
+        if(index == nums.length){ //base case
             if(fsum == ssum){
-                System.out.println(first + " : " + second); //base case
+                System.out.println(first + " : " + second);
             }
+            
             return;
         }
-        int item = nums[index];
+        
+        int item = nums[index];//item to be added
         
         first.add(item);
-        eqSum(first, second, fsum: fsum+item, ssum, nums, index:index+1);
-        first.remove(index:first.size()-1);
+        eqSum(first, second, fsum + item, ssum, nums, index + 1);
+        first.remove(first.size()-1);//backtrack, as its a list, .size instead of .length
         
         second.add(item);
-        eqSum(first, second, fsum, ssum: ssum+item, nums, index:index+1);
-        second.remove(index:second.size()-1);
+        eqSum(first, second, fsum, ssum + item, nums, index + 1);
+        second.remove(second.size()-1);
     }
 }
