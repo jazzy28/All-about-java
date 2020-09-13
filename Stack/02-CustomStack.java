8bb93a68fd4d260d38380dcc3d87d4814839c765
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-09-13 19:45:38 +0530

    Update 02-CustomStack.java

--- Stack/02-CustomStack.java
+++ Stack/02-CustomStack.java
@@ -1,1 +1,66 @@
+//Stack: Abstract data type (ADT), we know the working but not internal implementation
+//stack can be created with help of array, linkedlist, array list, queues
+
+//STACK USING ARRAY
+
+import java.util.*; 
+
+public class CustomStack 
+{
+	public static void main(String[] args) {
+	    
+	    //Creating stack with help of Array
+	    private int[] data; //array declaration
+	    private final int DEFAULT_SIZE = 10; //Size of array is 10
+	    
+	    //pointer to point the position remove/add element
+	    private int top = -1; 
+	    
+	    //constructor
+	    public CustomStack(){ //if size is not provided
+	        this.data = new int[DEFAULT_SIZE]; //array initialization
+	    }
+	    
+	    public CustomStack(int size){
+	        this.data = new int(size); //if size is provided
+	    }
+	    
+	    //to insert element
+	    public void push(int item){
+	        //to check if array is full or not
+	        if(isFull()){
+	            System.out.println("Stack overflow");
+	            return;
+	        }
+	        
+	        this.data[++top] = item; //before insertion, value of top will be increased
+	    }
+	    
+	    //to remove element
+	    public Integer pop(){ //returning the element
+	        if(isEmpty()){
+	            System.out.println("Stack underflow");
+	            return null; //don't write return -1, as there can be negative numbers too, so instead of int, make the return type as Integer
+	        }
+	        
+	        return this.data[top--] == item; //post decrement, first return the top most element, then decrease the value of top by 1
+	    }
+	    
+	    public boolean isFull(){
+	        return top == this.data.length-1; //pointing at the last element, return true/false
+	    }
+	    
+	     public boolean isEmpty(){
+	        return top == -1; //top = -1, when stack is empty
+	    }
+    //to display the top most element
+    public Integer peek(){
+	        if(isEmpty()){
+	            System.out.println("Stack is empty");
+	            return null;
+	    }
+	    return this.data[top];
+	    
+	}
+}
 
