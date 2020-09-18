27d423ce80a59d2b913e7ec6da02ff58d9327654
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-09-18 23:10:11 +0530

    Update 03-CircularQueue.java

--- Queues/03-CircularQueue.java
+++ Queues/03-CircularQueue.java
@@ -1,1 +1,59 @@
+import java.util.*;
 
+//CustomQueue.java
+public class CircularQueue
+{
+	public static void main(String[] args) {
+	   int[] data;
+	   int front, end;
+	   int size;
+	   private final int DEFAULT_SIZE = 1O; //final, so that value can't be changed further
+	   
+	   CircularQueue(){
+	       data = new int[DEFAULT_SIZE];
+	       front = -1;
+	       end = -1;
+	       size = 0; //new queue
+	   }
+	   
+	   CircularQueue(int initialSize){
+	       data = new int[initialSize];
+	       front = -1;
+	       end = -1;
+	       size = 0;
+	   }
+	   
+	   public boolean isFull(){
+	       return size == data.length;
+	   }
+	   
+	   public boolean isEmpty(){
+	       return size == 0;
+	   }
+	   
+	   public void add(int item){
+	       if(isFull()){
+	           System.out.println("Queue is full");
+	           return;
+	       }
+	       
+	       //updating the end first
+	       this.end = (end + 1) % data.length;
+	       this.data[end] = item;
+	       size++; //one element has been added
+	   }
+	   
+	   public Integer remove(){
+	       if(isEmpty()){
+	           System.out.println("Queue is empty");
+	           return null;
+	       }
+	       //storing before the front gets changed
+	       int temp = data[front];
+	       this.front = (front + 1)% data.length;
+	       size--;
+	       return temp;
+	   }
+	    
+	}
+}
